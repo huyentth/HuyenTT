2fddb95743c4263b7ababad94751a59ebb6dc25f
huyentth <huyentth.hust@gmail.com>
2021-06-01 14:58:35 +0700

    first commit

---
+++ 5_Huyen_Quiz/js/question.js
@@ -0,0 +1,27 @@
+const quiz = [
+    {
+        q: 'How many city are there in VietNam ?',
+        options:['58','60','62','64'],
+        answer:3
+    },
+    {
+        q: 'What city is Capital of Canada ?',
+        options:['Orlando','Toronto','Ottawa'],
+        answer:2
+    },
+    {
+        q: '3 + 4 = 7 ?',
+        options:['true', 'false'],
+        answer:0
+    },
+    {
+        q: 'How many members are there in BTS group?',
+        options:['5', '6','7'],
+        answer:2
+    },
+    {
+        q: '2 + 6 = ?',
+        options:['8','9','10','11'],
+        answer:0
+    }
+]
